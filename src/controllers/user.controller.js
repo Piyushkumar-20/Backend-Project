7cd5c88c0361b165f003a74de3cf7b0fa39c7495
Piyushkumar-20 <pk6686742@gmail.com>
2026-04-25 19:21:48 +0530

    🐛register controller bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, username, password } = req.body;
-  console.log("email: ", email);
+  //console.log("email: ", email);
 
   if (
     [fullname, email, username, password].some((field) => field?.trim() === "")
@@ -23,9 +23,10 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
-  console.log(req.files);
+  //console.log(req.files);
 
-  const avatarLocalPath = req.files?.avatar[0]?.path;
+const avatarLocalPath = req.files?.avatar?.[0]?.path;
+
   //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   let coverImageLocalPath;
